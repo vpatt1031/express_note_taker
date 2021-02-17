0482fe94a86d0039812682d42e2592bbf1545840
Vanessa Patterson <vpatt1031@gmail.com>
2021-02-17 09:08:00 -0500

    added Readme and updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const path = require('path')
-const fs = require('fs')
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const data = require("./db/db.json");
 
@@ -8,29 +8,50 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
-app.use(express.static("public"))
+app.use(express.json());
+app.use(express.static("public"));
 
 // Routes
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
-})
+// module.exports = (app) => {
 
-app.get("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf-8", (err, response) => {
-        if(err) throw err;
-        // console.log(response)
-        res.json(JSON.parse(response));
-    })
-    // res.json(data)
-})
+    app.get("/notes", (req, res) => {
+        res.sendFile(path.join(__dirname, "./public/notes.html"));
+    });
+    
+    app.get("/api/notes", (req, res) => {
+        fs.readFile("./db/db.json", "utf-8", (err, response) => {
+            if(err) throw err;
+            // console.log(response)
+            res.json(JSON.parse(response));
+        });
+        // res.json(data)
+    });
+//   };
+  
+      // Create New Note Items - takes in JSON input
+app.post('/api/notes', (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    const newListItem = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newListItem
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newListItem.routeName = newListItem.name.replace(/\s+/g, '').toLowerCase();
+    console.log(newListItem);
+  
+    data.push(newListItem);
+    res.json(newListItem);  
+});
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'));
+    });
+    
+    
+    app.listen(PORT, () => {
+        console.log("listening");
+    });
 
 
-app.listen(PORT, () => {
-    console.log("I'M ALIVE!!!")
-})
+
